8e7fb6dc802dbce3ca24f6ce2342768a18dcf771
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2018-12-30 21:28:18 -0500

    first commit

---
+++ Account.java
@@ -0,0 +1,17 @@
+//super class of so we can manipulae Accounts
+public class Account{
+  String fname="";
+  String lname=""
+  String empilid="";
+
+  public Account(){
+    
+  }
+  public Account(String empilid){
+    this.empilid=empilid;
+  }
+  public Account(String fname String lname){
+    this.fname=fname;
+    this.lname=lname;
+  }
+}
